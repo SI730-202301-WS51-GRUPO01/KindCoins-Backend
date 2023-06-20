f389244252eeb395488a68548b354a81f10f6e73
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-06-20 09:51:12 -0500

    TypeOfDonationResponse.cs

---
+++ KindCoins-Backend/KindCoins/Domain/Services/Communication/TypeOfDonationResponse.cs
@@ -0,0 +1,15 @@
+﻿using KindCoins_Backend.KindCoins.Domain.Models;
+using KindCoins_Backend.KindCoins.Shared.Domain.Services.Communication;
+
+namespace KindCoins_Backend.KindCoins.Domain.Services.Communication;
+
+public class TypeOfDonationResponse : BaseResponse<TypeOfDonation>
+{
+    public TypeOfDonationResponse(TypeOfDonation resource) : base(resource)
+    {
+    }
+
+    public TypeOfDonationResponse(string message) : base(message)
+    {
+    }
+}
